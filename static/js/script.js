e1a062cc2c22d37f30fd5a1ce1b3cf9881d1ac60
LinusLoof <linus.loof96@gmail.com>
2021-02-16 12:43:48 +0000

    note form and input fields to add_notes

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@
     $('.sidenav').sidenav();
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    // datepicker, output format, show clear button, Internationalization options:
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
