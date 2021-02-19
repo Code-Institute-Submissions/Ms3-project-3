f5802d9420f9ed051d1f42461f2020dfa4b0ab00
LinusLoof <linus.loof96@gmail.com>
2021-02-19 08:06:36 +0000

    Comments in css

--- static/js/script.js
+++ static/js/script.js
@@ -12,12 +12,13 @@
     });
   });
 
+// makes the navbar to disappear when scroll up and to apper when scroll down
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "-70px";
   }
 }
