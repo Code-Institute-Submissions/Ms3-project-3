83ea9a5d24f22fc9e031aabf92bf2a8357b8fec7
LinusLoof <linus.loof96@gmail.com>
2021-02-24 10:20:37 +0000

    changed app.py /register for it to work in heroku to register

--- app.py
+++ app.py
@@ -35,11 +35,10 @@ def register():
             flash("Username already exists")
             return redirect(url_for("register"))
 
-        register = {
+        mongo.db.users.insert_one({
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.users.insert_one(register)
+        })
 
         session["user"] = request.form.get("username").lower()
         flash("Register Successful!")
