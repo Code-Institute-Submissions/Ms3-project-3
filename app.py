5bdfd024ea2855807f2c2133f8ef7f255f576040
LinusLoof <linus.loof96@gmail.com>
2021-03-01 11:16:23 +0000

    Function documentation in app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+# Configuration
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
@@ -18,28 +19,31 @@ app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 
+# Home page
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
 
 
+# Register function
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # check username
+        # check if username already exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-
+        # If already exists flash...
         if existing_user:
             flash("Username already exists")
             return redirect(url_for("register"))
 
+        # sign up new user in db
         mongo.db.users.insert_one({
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
         })
-
+        # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Register Successful!")
         return redirect(url_for("profile", username=session["user"]))
@@ -47,19 +51,16 @@ def register():
     return render_template("register.html")
 
 
+# Login function
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        """
-        check username
-        """
+        # check if username exists in db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            """
-            ensure hashed password matches user input
-            """
+            # ensure hashed password matches user input
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
@@ -68,48 +69,52 @@ def login():
                     return redirect(url_for(
                         "profile", username=session["user"]))
             else:
-                """
-                invalid password match
-                """
+                # invalid password match
                 flash("Incorrect Username or Password")
                 return redirect(url_for("login"))
 
         else:
-            """
-            username doesn't exist
-            """
+            # username doesn't exist
             flash("Incorrect Username or Password")
             return redirect(url_for("login"))
 
     return render_template("login.html")
 
 
+# Profile function
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
+    # Check username
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
 
+    # If username match render profile.html
     if session["user"]:
         return render_template("profile.html", username=username)
-
+    # If not redirect to login
     return redirect(url_for("login"))
 
 
+# Find notes
 @app.route("/get_notes")
 def get_notes():
     notes = mongo.db.notes.find()
     return render_template("notes.html", notes=notes)
 
 
+# Logout function
 @app.route("/logout")
 def logout():
+    # Remove user from session cookies
     flash("You have been logged out")
     session.pop("user")
     return redirect(url_for("login"))
 
 
+# Add notes funtion
 @app.route("/add_note", methods=["GET", "POST"])
 def add_note():
+    # Post the strings to mongo db
     if request.method == "POST":
         is_urgent = "on" if request.form.get("is_urgent") else "off"
         note = {
@@ -126,8 +131,10 @@ def add_note():
     return render_template("add_notes.html")
 
 
+# Edit notes function
 @app.route("/edit_note/<note_id>", methods=["GET", "POST"])
 def edit_note(note_id):
+    # Post the strings to mongo db
     if request.method == "POST":
         is_urgent = "on" if request.form.get("is_urgent") else "off"
         submit = {
@@ -137,6 +144,7 @@ def edit_note(note_id):
             "is_urgent": is_urgent,
             "created_by": session["user"]
         }
+        # Update the strings in mongo db
         mongo.db.notes.update({"_id": ObjectId(note_id)}, submit)
         flash("Note Updated!")
 
@@ -144,8 +152,10 @@ def edit_note(note_id):
     return render_template("edit_note.html", note=note)
 
 
+# Delete function
 @app.route("/delete_note/<note_id>")
 def delete_note(note_id):
+    # Remove ObjectId from mongo db
     mongo.db.notes.remove({"_id": ObjectId(note_id)})
     flash("Note Deleted!")
     return redirect(url_for("get_notes"))
